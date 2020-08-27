cd37220b070f501747ea11b1fede8199bcdcfe47
GaelTouquet <Gael.Touquet@creatis.insa-lyon.fr>
2020-08-27 16:35:35 +0200

    Moussavi compiles but needs some work

--- phase_correction_basedon_MoussaviMRM2014_clean.m
+++ phase_correction_basedon_MoussaviMRM2014_clean.m
@@ -1,6 +1,6 @@
 % addpath('D:\Matlab_functions')
 % addpath(('D:\Projects_D\SequenceDevelopement\CODES_2018\'))
-addpath(genpath('C:\Users\gaelr\OneDrive\Documents\MATLAB\codes_4_gael\CODES\'))
+addpath(genpath('.\CODES\'))
 % addpath(('D:\Projects_D\SequenceDevelopement\CODES_2018\Code_bioeng022\'))
 % addpath(('D:\Projects_D\SequenceDevelopement\CODES_2018\com_computation\'))
 % addpath(genpath('D:\Projects_D\SequenceDevelopement\CODES_2018\MRecon-3.0.553\'))
@@ -8,15 +8,8 @@ addpath(genpath('C:\Users\gaelr\OneDrive\Documents\MATLAB\codes_4_gael\CODES\'))
 % addpath(genpath('D:\Projects_D\SequenceDevelopement\CODES_2018\imagine\'))
 % addpath(genpath('D:\Projects_D\SequenceDevelopement\CODES_2018\gpuNUFFT-master'))
 
-%% antiparallel spoke tests data
-datafolder = 'C:\Users\gaelr\OneDrive\Documents\MATLAB\codes_4_gael\DATA_PH\';
-
-filename = 'ph_11072019_2024583_3_2_wip_koosh_pcV4';
-load([datafolder filename '_kdata']); load([datafolder filename '_channel_images_regrid']); 
-load([datafolder filename '_k']); load([datafolder filename '_dcf'])
-kdata_koosh = kdata; kdata_koosh = kdata_koosh(:,:,:,1);
-k_koosh = k; dcf_koosh = dcf; chims_koosh = squeeze(channel_images);
-[nx_k,ntviews_k,nz_k,nc_k,npc_k] = size(kdata_koosh);
+%% define data
+datafolder = '.\DATA_PH\';
 
 filename = 'ph_11072019_2035184_5_2_wip_phyllo_classicV4';
 load([datafolder filename '_coils']);
@@ -27,101 +20,147 @@ load([datafolder filename '_k']); load([datafolder filename '_dcf'])
 kdata_phyllo = kdata; k_phyllo = k; dcf_phyllo = dcf; chims_phyllo = squeeze(channel_images);
 [nx,ntviews,nz,nc,npc] = size(kdata_phyllo);
 
-filename = 'ph_11072019_2130137_13_2_wip_phyllo_classic_antiparV4';
-load([datafolder filename '_kdata']); load([datafolder filename '_channel_images_regrid']); 
-load([datafolder filename '_k']); load([datafolder filename '_dcf'])
-kdata_phylloanti = kdata; k_phylloanti = k; dcf_phylloanti = dcf; chims_phylloanti = squeeze(channel_images);
-[nx,ntviews,nz,nc,npc] = size(kdata_phylloanti);
 
 %% compute the phase of the central point of k-space
-phase_koosh = angle(kdata_koosh(size(kdata_koosh,1)/2,:,:,:)); 
 phase_phyllo = angle(kdata_phyllo(size(kdata_phyllo,1)/2,:,:,:));
-phase_phylloanti = angle(kdata_phylloanti(size(kdata_phylloanti,1)/2,:,:,:));
 
-[phi_koosh, theta_koosh, kpos_koosh] = compute_phyllo_angles(kdata_koosh,0);
 [phi_phyllo, theta_phyllo, kpos_phyllo] = compute_phyllo_angles(kdata_phyllo,0);
-figure;scatter3(reshape(kpos_koosh(:,:,1),[ntviews_k*nz_k 1]),reshape(kpos_koosh(:,:,2),[ntviews_k*nz_k 1]),reshape(kpos_koosh(:,:,3),[ntviews_k*nz_k 1]));axis equal
-figure;scatter3(reshape(kpos_phyllo(2:end,:,1),[(ntviews-1)*nz 1]),...
-    reshape(kpos_phyllo(2:end,:,2),[(ntviews-1)*nz 1]),...
-    reshape(kpos_phyllo(2:end,:,3),[(ntviews-1)*nz 1]));axis equal
 
-figure; scatter3(reshape(phi_phyllo,[ntviews*nz 1]),reshape(theta_phyllo,[ntviews*nz 1]),reshape(squeeze(phase_phyllo(:,:,:,7)),[ntviews*nz 1]));
-xlabel('phi');ylabel('theta');zlabel('phase')
-%% phi going from 0 to 360
-coil = 1;
-figure; scatter3((reshape(phi_koosh,[ntviews_k*nz_k 1])),reshape(theta_koosh,[ntviews_k*nz_k 1]),reshape(squeeze(phase_koosh(:,:,:,coil)),[ntviews_k*nz_k 1]),10,reshape(squeeze(phase_koosh(:,:,:,coil)),[ntviews_k*nz_k 1]));
-xlabel('phi (rad)');ylabel('theta (rad)');zlabel('phase (rad)'); view(-25,10); title(['Koosh - Coil #' num2str(coil)]);colorbar; % caxis([-3.1416/4 3.1415/4]);
-figure; scatter3((reshape(phi_phyllo,[ntviews*nz 1])),reshape(theta_phyllo,[ntviews*nz 1]),reshape(squeeze(phase_phyllo(:,:,:,coil)),[ntviews*nz 1]),10,reshape(squeeze(phase_phyllo(:,:,:,coil)),[ntviews*nz 1]));
-xlabel('phi (rad)');ylabel('theta (rad)');zlabel('phase (rad)'); view(-25,10); title(['Phyllo - Coil #' num2str(coil)]);colorbar; % caxis([-3.1416/4 3.1415/4]);
-% figure; scatter3(rem(reshape(phi_phyllo,[ntviews*nz 1]),pi*2),reshape(theta_phyllo,[ntviews*nz 1]),reshape(squeeze(phase_phylloanti(:,:,:,coil)),[ntviews*nz 1]),10,reshape(squeeze(phase_phylloanti(:,:,:,coil)),[ntviews*nz 1]));
-% xlabel('phi (rad)');ylabel('theta (rad)');zlabel('phase (rad)'); view(-25,10); title(['Phyllo anti - Coil #' num2str(coil)]);colorbar; % caxis([-3.1416/4 3.1415/4]);
-coil = 1;
-figure; 
-subplot(121);scatter3(rem(reshape(phi_koosh,[ntviews_k*nz_k 1]),pi*2),reshape(theta_koosh,[ntviews_k*nz_k 1]),reshape(squeeze(phase_koosh(:,:,:,coil)),[ntviews_k*nz_k 1]),10,reshape(squeeze(phase_koosh(:,:,:,coil)),[ntviews_k*nz_k 1]));
-xlabel('phi (rad)');ylabel('theta (rad)');zlabel('phase (rad)'); view(-25,10); title(['Koosh - Coil #' num2str(coil)]);colorbar; % caxis([-3.1416/4 3.1415/4]);
-subplot(122); scatter3(rem(reshape(phi_phyllo,[ntviews*nz 1]),pi*2),reshape(theta_phyllo,[ntviews*nz 1]),reshape(squeeze(phase_phyllo(:,:,:,coil)),[ntviews*nz 1]),10,reshape(squeeze(phase_phyllo(:,:,:,coil)),[ntviews*nz 1]));
-xlabel('phi (rad)');ylabel('theta (rad)');zlabel('phase (rad)'); view(-25,10); title(['Phyllo - Coil #' num2str(coil)]);colorbar; % caxis([-3.1416/4 3.1415/4]);
-% %% first try the error model for a single interleaf
-% ileaf = 1;
-% x1 = phi_phyllo(ileaf,:); x2 = theta_phyllo(ileaf,:); x = [x1(:),x2(:)];
-% Y = squeeze(phase_phyllo(:,ileaf,:,1)); Y = Y(:);
+figure;
+x1 = rem(reshape(phi_phyllo,[ntviews*nz 1]),pi*2); x2 = reshape(theta_phyllo,[ntviews*nz 1]); x = [x1(:),x2(:)];
+
+for coil = 1:27
+    Y(:,coil) = reshape(squeeze(phase_phyllo(:,:,:,coil)),[ntviews*nz 1]);
+    subplot(3,9,coil); scatter3(x(:,1),x(:,2),Y(:,coil),1,Y(:,coil));
+    xlabel('phi (rad)');ylabel('theta (rad)');zlabel('phase (rad)'); title(['Phyllo - Coil #' num2str(coil)]); % caxis([-3.1416/4 3.1415/4]);
+end
+%%  
+%!!!TODO find a way to make the phase planes fittable (for now when plane
+%goes through +/-pi it is not fittable as it is not continuous anymore
+% phase_phyllo = arrayfun(@(x) (x<=0).*(x+2*pi) + (x>0).*(x),phase_phyllo)
 % 
-% errmodel3D = @(coeffs,x) coeffs(1).*cos(x(:,1)).*sin(x(:,2)) + ...
-%     coeffs(2).*sin(x(:,1)).*sin(x(:,2)) + coeffs(3).*cos(x(:,2)) + coeffs(4);
-% % errmodel3D = @(coeffs,x) coeffs(1).*cos(coeffs(7).*x(:,1) + coeffs(2)).*cos(coeffs(8).*x(:,2) + coeffs(3)) + coeffs(4).*x(:,1) + coeffs(5).*x(:,2) + coeffs(6)  
+% figure;
+% x1 = rem(reshape(phi_phyllo,[ntviews*nz 1]),pi*2); x2 = reshape(theta_phyllo,[ntviews*nz 1]); x = [x1(:),x2(:)];
 % 
-% opts = statset('nlinfit');
-% opts.RobustWgtFun = 'andrews';
-% opts.TolFun = 10^-12;
-% opts.MaxFunEvals = 10^5;
-% [coeffs3D(:,ileaf),R,J,CovB,MSE,ErrorModelInfo] = nlinfit(x,Y(:),errmodel3D,[0.;0.;0.;0.;0.;2.3798;0;0],opts);
-% %% 
-% 
-% Yfit = errmodel3D(coeffs3D(:,ileaf),x);
-% 
-% figure;scatter3(rem(x(:,1),pi*2),x(:,2),Y);
-% hold on
-% scatter3(rem(x(:,1),pi*2),x(:,2),Yfit,'*k')
-%% try the error model for each interleaf
-for ileaf = 1:8
-    x1 = phi_phyllo(ileaf,:); x2 = theta_phyllo(ileaf,:); x = [x1(:),x2(:)];
-    Y = squeeze(phase_phyllo(:,ileaf,:,1)); Y = Y(:);
-    
+% for coil = 1:8
+%     Y(:,coil) = reshape(squeeze(phase_phyllo(:,:,:,coil)),[ntviews*nz 1]);
+%     subplot(240+coil); scatter3(x(:,1),x(:,2),Y(:,coil),10,Y(:,coil));
+%     xlabel('phi (rad)');ylabel('theta (rad)');zlabel('phase (rad)'); view(-25,10); title(['Phyllo - Coil #' num2str(coil)]);colorbar; % caxis([-3.1416/4 3.1415/4]);
+% end
+
+%% try the error model for each coil
+figure;
+for coil = 1:1
+%     Y = reshape(squeeze(phase_phyllo(:,:,:,coil)),[ntviews*nz 1]); Y = Y(:);
     errmodel3D = @(coeffs,x) coeffs(1).*cos(x(:,1)).*sin(x(:,2)) + ...
-    coeffs(2).*sin(x(:,1)).*sin(x(:,2)) + coeffs(3).*cos(x(:,2)) + coeffs(4);
-
+        coeffs(2).*sin(x(:,1)).*sin(x(:,2)) + coeffs(3).*cos(x(:,2)) + coeffs(4);
+%     following mode was tried but doesn't fit, probably because iti is a
+%     non-linear model in a linear fit
+%     errmodel3D = @(coeffs,x) mod( coeffs(1).*cos(x(:,1)).*sin(x(:,2)) + ...
+%     coeffs(2).*sin(x(:,1)).*sin(x(:,2)) + coeffs(3).*cos(x(:,2)) + coeffs(4),2*pi)-pi;
     opts = statset('nlinfit');
     opts.RobustWgtFun = 'andrews';
     opts.TolFun = 10^-12;
     opts.MaxFunEvals = 10^5;
-    [coeffs3D(:,ileaf),R,J,CovB,MSE] = nlinfit(x,Y(:),errmodel3D,[0,0,0,0],opts);
+    [coeffs3D(:,coil),R,J,CovB,MSE] = nlinfit(x,Y(:,coil),errmodel3D,[0,0,0,0],opts);
+    Yfit(:,coil) = errmodel3D(coeffs3D(:,coil),x);
     
-    Yfit = errmodel3D(coeffs3D(:,ileaf),x);
-
-    figure;scatter3(x(:,1),x(:,2),Y);
+    % plotting time
+    subplot(240+coil); scatter3(x(:,1),x(:,2),Y(:,coil),10,Y(:,coil));
     hold on
-    scatter3(x(:,1),x(:,2),Yfit,'*k');
-      
-    clear x Y Yfit
+    scatter3(x(:,1),x(:,2),Yfit(:,coil),'*k');
+    xlabel('phi (rad)');ylabel('theta (rad)');zlabel('phase (rad)'); view(-25,10); title(['Phyllo - Coil #' num2str(coil)]);colorbar;
+%     figure;scatter3(x(:,1),x(:,2),Y);
+%     hold on
+%     scatter3(x(:,1),x(:,2),Yfit,'*k');
+%     clear Y Yfit
 end
 
-for ileaf = 1:8
-    corrfact = coeffs3D(1,ileaf)*kpos_phyllo(ileaf,:,1) + coeffs3D(2,ileaf)*kpos_phyllo(ileaf,:,2) +coeffs3D(3,ileaf)*kpos_phyllo(ileaf,:,3);
-    corrfact = permute(corrfact,[3 1 2]);
-    corrfact = repmat(corrfact,[272 1 1]);
-    Scorr(:,ileaf,:) = kdata_phyllo(:,ileaf,:,coil).*exp(-i*corrfact);
-    clear corrfact
-end
-% 
+%% uncorrected image
+coil=1;
 k = reshape(k_phyllo,[nx*ntviews*nz 3]);
 dcf = reshape(dcf_phyllo,[nx*ntviews*nz 1]);
-rawdata = kdata_phyllo; rawdata = reshape(rawdata,[nx*ntviews*nz nc]);
 precision = 1E-2;
 siz = [180 180 180];
-for coil = 1:nc
-    k2im(:,:,:,coil) = nufft3_type1(double(k), double(rawdata(:,coil).*dcf), siz, +1,precision);
-end
-images = sum(k2im .* conj(csm),4)./coil_rss;
+rawdata = reshape(kdata_phyllo,[nx*ntviews*nz nc]);
+uncorr_image(:,:,:,coil) = nufft3_type1(double(k), double(rawdata(:,coil).*dcf), siz, +1,precision);
+clear rawdata
+%% correction
+corrfact = coeffs3D(1,coil)*kpos_phyllo(:,:,1) + coeffs3D(2,coil)*kpos_phyllo(:,:,2) + coeffs3D(3,coil)*kpos_phyllo(:,:,3);% + coeffs3D(4,coil);
+corrfact = permute(corrfact,[3 1 2]);
+corrfact = repmat(corrfact,[272 1 1]);
+Scorr(:,:,:,coil) = kdata_phyllo(:,:,:,coil).*exp(-i*corrfact);
+clear corrfact
+corrected_S(:,coil) = reshape(Scorr(:,:,:,coil),[nx*ntviews*nz 1]);
+clear Scorr
+%% corrected image
+
+corr_image(:,:,:,coil) = nufft3_type1(double(k), double(corrected_S(:,coil).*dcf), siz, +1,precision);
+
+
+%% display images
+imagine(uncorr_image(:,:,:,1));
+imagine(corr_image(:,:,:,1));
+%% 
+
+
+% corrfact = permute(corrfact,[3 1 2]);
+% corrfact = repmat(corrfact,[272 1 1]);
+% Scorr(:,ileaf,:) = kdata_phyllo(:,ileaf,:,coil).*exp(-i*corrfact);
+
+% corrected image
+
+% %% try the error model for each interleaf
+% for ileaf = 1:8
+%     x1 = phi_phyllo(ileaf,:); x2 = theta_phyllo(ileaf,:); x = [x1(:),x2(:)];
+%     Y = squeeze(phase_phyllo(:,ileaf,:,1)); Y = Y(:);
+%     
+%     errmodel3D = @(coeffs,x) coeffs(1).*cos(x(:,1)).*sin(x(:,2)) + ...
+%     coeffs(2).*sin(x(:,1)).*sin(x(:,2)) + coeffs(3).*cos(x(:,2)) + coeffs(4);
+% 
+%     opts = statset('nlinfit');
+%     opts.RobustWgtFun = 'andrews';
+%     opts.TolFun = 10^-12;
+%     opts.MaxFunEvals = 10^5;
+%     [coeffs3D(:,ileaf),R,J,CovB,MSE] = nlinfit(x,Y(:),errmodel3D,[0,0,0,0],opts);
+%     
+%     Yfit = errmodel3D(coeffs3D(:,ileaf),x);
+% 
+%     figure;scatter3(x(:,1),x(:,2),Y);
+%     hold on
+%     scatter3(x(:,1),x(:,2),Yfit,'*k');
+%       
+%     clear x Y Yfit
+% end
+% 
+% for ileaf = 1:8
+%     corrfact = coeffs3D(1,ileaf)*kpos_phyllo(ileaf,:,1) + coeffs3D(2,ileaf)*kpos_phyllo(ileaf,:,2) +coeffs3D(3,ileaf)*kpos_phyllo(ileaf,:,3);
+%     corrfact = permute(corrfact,[3 1 2]);
+%     corrfact = repmat(corrfact,[272 1 1]);
+%     Scorr(:,ileaf,:) = kdata_phyllo(:,ileaf,:,coil).*exp(-i*corrfact);
+%     clear corrfact
+% end
+% % 
+% k = reshape(k_phyllo,[nx*ntviews*nz 3]);
+% dcf = reshape(dcf_phyllo,[nx*ntviews*nz 1]);
+% rawdata = kdata_phyllo; rawdata = reshape(kdata_phyllo,[nx*ntviews*nz nc]);
+% precision = 1E-2;
+% siz = [180 180 180];
+% for coil = 1:nc
+%     k2im(:,:,:,coil) = nufft3_type1(double(k), double(rawdata(:,coil).*dcf), siz, +1,precision);
+% end
+% images = sum(k2im .* conj(csm),4)./coil_rss;
+
+
+
+
+
+
+
+
+
+
+
 % G = gpuNUFFT(double(k)',[],2,7,10,[180 180 180],[],true); 
 % imags = G'*(rawdata.*repmat(dcf,[1 nc])); imags_phyllo = sqrt(sum(imags.*conj(imags),4)); imagine(imags_phyllo)
 % 
