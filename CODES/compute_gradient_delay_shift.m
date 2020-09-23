95c65eca4ea54a825d94046cda0b4e7c0420f4f0
GaelTouquet <gael.touquet@gmail.com>
2020-09-23 10:50:28 +0200

    needs testing

--- CODES/compute_gradient_delay_shift.m
+++ CODES/compute_gradient_delay_shift.m
@@ -1,15 +1,32 @@
 function [ shift ] = compute_gradient_delay_shift( spoke1,spoke2 )
 %COMPUTE_GRADIENT_DELAY_SHIFT Summary of this function goes here
 %   Detailed explanation goes here
-S1 = spoke1;
-S2 = flip(spoke2);
-F1 = fft(S1);
-F2 = fft(S2);
-g = F1.*conj(F2);
-phase = angle(g);
-test1 = fft(g);
-test2 = ifft(g);
-k = diff(phase);
 
+f1 = fft(abs(spoke1));
+f2 = fft(flip(abs(spoke2)));
+
+% f = Fs*(0:(L/2))/L;
+
+g = f1.*conj(f2);
+af1 = abs(fft(spoke1));
+[M,I] = max(af1);
+
+upval = M;
+downval = M;
+imin = I;
+imax = I;
+
+while downval>M*0.1 && upval>M*0.1
+    imin = imin-1;
+    imax = imax+1;
+    downval = af1(imin);
+    upval = af1(imax);
+end
+
+phases = angle(g(imin:imax));
+
+dir = phases\reshape((imin:imax),size(phases));
+
+shift = dir * 0.5 / (272 * 2 * pi);
 end
 
--- CODES/gradient_delay_compensation.m
+++ CODES/gradient_delay_compensation.m
@@ -2,50 +2,28 @@ function [ kdata_corr ] = gradient_delay_compensation( kdata )
 %GRADIENT_DELAY_COMPENSATION Corrects radial data for gradient delays
 %   from https://cds.ismrm.org/protected/11MProceedings/files/2816.pdf
 
-% [nreadouts,nspokes,ninterleaves,ncoils,nphasecontrast] = size(kdata_phyllo);
+[nreadouts,nspokes,ninterleaves,ncoils] = size(kdata);
 
-spoke1 = kdata(:,7,2483,1);
-spoke2 = kdata(:,6,2448,1);
+for i_coil = 1:ncoils
 
-f1 = fft(abs(spoke1));
-f2 = fft(flip(abs(spoke2)));
+    spoke1 = kdata(:,7,2483,i_coil);
+    spoke2 = kdata(:,6,2448,i_coil);
 
-% f = Fs*(0:(L/2))/L;
+    xshift = compute_gradient_delay(spoke1,spoke2);
 
-g = f1.*conj(f2);
-af1 = abs(fft(spoke1));
-[M,I] = max(af1);
 
-upval = M;
-downval = M;
-imin = I;
-imax = I;
+    spoke1 = kdata(:,7,2375,i_coil);
+    spoke2 = kdata(:,6,2573,i_coil);
 
-while downval>M*0.1 && upval>M*0.1
-    imin = imin-1;
-    imax = imax+1;
-    downval = af1(imin);
-    upval = af1(imax);
+    yshift = compute_gradient_delay(spoke1,spoke2);
+
+
+    spoke1 = kdata(:,1,2508,i_coil);
+    spoke2 = kdata(:,2,158,i_coil);
+
+    zshift = compute_gradient_delay(spoke1,spoke2);
+
 end
-
-phases = angle(g(imin:imax));
-
-dir = phases\reshape((imin:imax),size(phases));
-
-linshift = dir * 0.5 / (272 * 2 * pi);
-% 
-% S1 = abs(Spoke1);
-% S2 = abs(flip(Spoke2));
-% F1 = fft(S1);
-% F2 = fft(S2);
-% g = F1.*conj(F2);
-% phase = angle(g);
-% k = diff(phase);
-% 
-% 
-% for i_coil = 1:n_coils:
-%     spoke1 = 
-% end
 
 end
 
